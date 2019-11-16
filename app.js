048ace41791199eaf8ba4347c94a00e717f17b0f
Salem Eid <salem.eid@gmail.com>
2019-11-16 15:52:30 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,157 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//---------------------------------------------------------
+// connect mongo db
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false });
+//creating Schema
+const itemsSchema = {
+  name: String
+};
+//
+// creating model with schema
+const Item = mongoose.model("Item", itemsSchema);
+
+//declaring datas
+const item1 =  new Item ({
+  name: "Add new items"
+})
+
+const item2 = new Item ({
+  name: "<- click checkbox to delete"
+})
+
+const item3 = new Item ({
+  name: "extra items"
+})
+
+// consolidate data into one const
+const defaultItems = [item1,item2,item3];
+// const defaultItems = [];
+// custom list schema
+const listSchema = {
+  name : String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+//inserting data to data mdoel.
+
+
+//--------------------------------Homepage-----------------------------------
+app.get("/", function(req, res) {
+
+  Item.find({},function(err,foundItems){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems,function(err){
+        if(err) {
+          console.log("failed to insert data");
+        } else {
+          console.log("Success Insertion Data");
+        }
+      });
+      res.redirect("/")
+    } else {
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+   }
+
+ });
+
+
+
+ //----------------Dynamic Route---page-------
+ app.get("/:customList",function(req,res){
+   const customList = _.capitalize(req.params.customList);
+
+   List.findOne({name: customList}, function(err, foundList){
+     if(!err){
+       if (!foundList){
+         const list = new List({
+           name: customList,
+           items: defaultItems
+         });
+         list.save();
+         res.redirect("/" + customList);
+
+       } else {
+         //Show an existing list.
+         res.render("list",{listTitle: foundList.name, newListItems: foundList.items})
+       }
+     }
+   });
+ });
+});
+
+//----------------new items
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item ({
+    name: itemName
+  });
+
+  if (listName === "Today"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName},function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+
+      res.redirect("/" + listName);
+    });
+  };
+
+
+});
+//----------------delete items
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    //----- mongoose find and remove by ID .
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if(!err){
+        console.log("Deleted");
+
+        res.redirect("/");
+      }
+    });
+  } else {
+        console.log(listName);
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+          if (!err) {
+            console.log("Deleted Custom List Iemm");
+            res.redirect("/" + listName);
+          } else {
+            console.log(err);
+            console.log(listName);
+          };
+        });
+      };
+    });
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+
+//-----------------------------
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
