fb55f663b046e7a4a8ba909633e34ccad7ded9dd
Salem Eid <salem.eid@gmail.com>
2019-11-19 10:47:46 +0300

    error Procfile adjusted

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ app.get("/about", function(req, res){
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
-}
+};
 
 app.listen(port, function() {
   console.log("Server started on port" + port);
